af31b2f4b76cc9c61abb9b1ecb0de7f657d6361e
Baptiste Dumont <baptiste1.dumont@gmail.com>
2021-09-02 11:20:22 +0200

    Update firebase.js

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -4,14 +4,14 @@ import 'firebase/firestore';
 
 // firebase init - add your own config here
 const firebaseConfig = {
-    apiKey: "AIzaSyBwVg9HQAZSf1UDjXQoJeNOtWI3imjDtEE",
-    authDomain: "thecountofmoney-11.firebaseapp.com",
-    databaseURL: "https://thecountofmoney-11.firebaseio.com",
-    projectId: "thecountofmoney-11",
-    storageBucket: "thecountofmoney-11.appspot.com",
-    messagingSenderId: "229187658352",
-    appId: "1:229187658352:web:edfa4dd23a7bac36600ab5",
-    measurementId: "G-HF4SZC2HKE"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 firebase.initializeApp(firebaseConfig);
 
